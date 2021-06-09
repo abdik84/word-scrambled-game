8634069479e45b6f42a6fd3e5269228fcf1e1281
Abdi Kurniawan <abdikurniawan060395@gmail.com>
2021-06-09 14:02:49 +0700

    create relation to game detail

--- app/Models/Games.php
+++ app/Models/Games.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Games extends Model
 {
     use HasFactory;
+
+    public function details() {
+        return $this->hasMany('App\Models\GamesDetail', 'games_id');
+    }
 }
